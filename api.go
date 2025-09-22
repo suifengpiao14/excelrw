a07f7ce75ba3f23c8e09e89ae85fd5c91f61fa92
zheng peng <pengzheng@huishoubao.com.cn>
2025-09-22 09:59:49 +0800

    optimize

--- api.go
+++ api.go
@@ -3,80 +3,22 @@ package excelrw
 import (
 	"context"
 	"encoding/json"
-	"fmt"
-	"strings"
+	"regexp"
 	"time"
 
 	"github.com/go-playground/validator/v10"
 	"github.com/pkg/errors"
 	"github.com/spf13/cast"
-	"github.com/suifengpiao14/jsonpathmap"
+	"github.com/suifengpiao14/apihttpprotocol"
 	"github.com/tidwall/gjson"
 	"github.com/tidwall/sjson"
-	"gitlab.huishoubao.com/gopackage/apihttpprotocol"
-	"gitlab.huishoubao.com/gopackage/apihttpprotocol/clientprotocol"
-)
-
-type SimpleExportOut struct {
-	Url string `json:"url"`
-}
-
-/*
-|request|object | 是  | 无 | 代理请求参数|
-|request.url|string | 是  | 无 | 代理请求地址 |
-|request.method|string | 是  | 无 | 代理请求方法 |
-|request.headers|string | 是  | 无 | 代理请求头 |
-|request.body|string | 是  | 无 | 代理请求body 体（页码参数必须使用{{pageIndex}}占位符） |
-*/
-
-type ProxyRquest struct {
-	Url             string                          `json:"url"  validate:"required"`
-	Method          string                          `json:"method" validate:"required"`
-	Headers         map[string]string               `json:"headers"`
-	Body            json.RawMessage                 `json:"body" validate:"required"`
-	PageIndexPath   string                          `json:"pageIndexPath"` //页码参数路径，例如：$.data.pageIndex
-	PageSizePath    string                          `json:"pageSizePath"`  //每页大小参数路径，例如：$.data.pageSize
-	MiddlewareFuncs apihttpprotocol.MiddlewareFuncs `json:"-"`             // 请求中间件函数列表，一般可以使用动态脚本生成
-}
-type ProxyResponse struct {
-	DataPath        string                          `json:"dataPath"  validate:"required"`
-	BusinessOkJson  json.RawMessage                 `json:"businessOkJson"` //业务成功标识json字符串，例如：{"code":0}
-	MiddlewareFuncs apihttpprotocol.MiddlewareFuncs `json:"-"`              // 请求中间件函数列表，一般可以使用动态脚本生成
-}
-type Settings struct {
-	Filename        string        `json:"filename" validate:"required"` //导出文件全称如 /static/export/20231018_1547.xlsx
-	FieldMetas      FieldMetas    `json:"fieldMetas"`                   //字段映射信息{"id":"ID","name":"姓名"}
-	Interval        time.Duration `json:"interval"`
-	DeleteFileDelay time.Duration `json:"deleteFileDelay"`
-}
-
-type ExportApiIn struct {
-	ProxyRquest   ProxyRquest   `json:"proxyRequest" validate:"required"`  //请求数据参数
-	ProxyResponse ProxyResponse `json:"proxyResponse" validate:"required"` //响应数据参数
-	Settings      Settings      `json:"settings" validate:"required"`      //配置信息
-}
-
-const (
-	maxLoopTimes = 5000 //最大循环次数，防止死循环
 )
 
 // 导出到Excel文件Api ，可直接对接http请求
-func ExportApi(in ExportApiIn) (err error) {
+func ExportApi(in ExportApiIn) (errChan chan error, err error) {
 	err = validator.New().Struct(in)
 	if err != nil {
-		return err
-	}
-	var businessOkkvs jsonpathmap.PathValues
-	if len(in.ProxyResponse.BusinessOkJson) > 0 {
-		var businessOkJson any
-		err = json.Unmarshal(in.ProxyResponse.BusinessOkJson, &businessOkJson)
-		if err != nil {
-			return err
-		}
-		businessOkkvs, err = jsonpathmap.FlattenJSON(businessOkJson)
-		if err != nil {
-			return err
-		}
+		return nil, err
 	}
 
 	ctx := context.Background()
@@ -93,26 +35,30 @@ func ExportApi(in ExportApiIn) (err error) {
 	filename := settings.Filename
 	fieldMetas := in.Settings.FieldMetas
 	ecw := NewExcelStreamWriter(ctx, filename, fieldMetas)
-	ecw = ecw.WithInterval(settings.Interval).WithDeleteFile(deleteFileDelay, nil).WithFetcher(func(loopCount int) (rows []map[string]string, forceBreak bool, err error) {
-		if maxLoopTimes < loopCount {
-			err = fmt.Errorf("too many loops")
-			return nil, false, err
-		}
-
+	startIndex := -1
+	startIndexRaw := ""
+	exp := regexp.MustCompile(`\d+`)
+	ecw = ecw.WithInterval(settings.Interval).WithDeleteFile(deleteFileDelay, nil).WithMaxLoopCount(maxLoopTimes).WithFetcher(func(loopCount int) (rows []map[string]string, forceBreak bool, err error) {
 		if proxyReq.PageIndexPath == "" { //不带页码占位符，则只获取一次数据
 			forceBreak = true
 		}
 		body := proxyReq.Body
 		pageIndexDelta := loopCount - 1
-		if proxyReq.PageSizePath != "" { //带每页大小占位符，则只获取一次数据
-			pageIndex := gjson.GetBytes(body, proxyReq.PageIndexPath).Int() + int64(pageIndexDelta)
-			body, err = sjson.SetBytes(body, proxyReq.PageSizePath, pageIndex)
+		if proxyReq.PageIndexPath != "" { //带每页大小占位符，则只获取一次数据
+			if startIndex == -1 {
+				result := gjson.GetBytes(body, proxyReq.PageIndexPath)
+				startIndex = int(result.Int())
+				startIndexRaw = result.Raw
+			}
+			pageIndex := startIndex + pageIndexDelta
+			raw := exp.ReplaceAllString(startIndexRaw, cast.ToString(pageIndex)) // 确保类型一致
+			body, err = sjson.SetRawBytes(body, proxyReq.PageIndexPath, []byte(raw))
 			if err != nil {
 				return nil, forceBreak, err
 			}
 		}
 
-		client := clientprotocol.NewRestyClientProtocol(proxyReq.Method, proxyReq.Url)
+		client := apihttpprotocol.NewClientProtocol(proxyReq.Method, proxyReq.Url)
 		client.Request().MiddlewareFuncs.Add(proxyReq.MiddlewareFuncs...)
 		client.Response().MiddlewareFuncs.Add(proxyRsp.MiddlewareFuncs...)
 		client.Request().SetHeader("Content-Type", "application/json")
@@ -121,11 +67,10 @@ func ExportApi(in ExportApiIn) (err error) {
 		if err != nil {
 			return nil, forceBreak, err
 		}
-		for _, okkv := range businessOkkvs {
-			val := gjson.GetBytes(resp, okkv.Path).String()
-			expectedVal := cast.ToString(okkv.Value)
-			if !strings.EqualFold(val, expectedVal) {
-				err = errors.Errorf("business error: expatted:%s,got:%s,response:%s", expectedVal, val, string(resp))
+		if in.ProxyResponse.BusinessCodePath != "" {
+			businessCode := gjson.GetBytes(resp, in.ProxyResponse.BusinessCodePath).String()
+			if businessCode != cast.ToString(in.ProxyResponse.BusinessOkCode) {
+				err = errors.Errorf("business error: expatted:%s,got:%s,url:%s,response:%s", in.ProxyResponse.BusinessOkCode, businessCode, proxyReq.Url, string(resp))
 				return nil, forceBreak, err
 			}
 		}
@@ -147,13 +92,50 @@ func ExportApi(in ExportApiIn) (err error) {
 		}
 		return items, forceBreak, nil
 	})
-	errChan, err := ecw.Run()
-	if err != nil {
-		return err
-	}
-	err = <-errChan
-	if err != nil {
-		return err
-	}
-	return nil
+	errChan, err = ecw.Run()
+	return errChan, err
 }
+
+type SimpleExportOut struct {
+	Url string `json:"url"`
+}
+
+/*
+|request|object | 是  | 无 | 代理请求参数|
+|request.url|string | 是  | 无 | 代理请求地址 |
+|request.method|string | 是  | 无 | 代理请求方法 |
+|request.headers|string | 是  | 无 | 代理请求头 |
+|request.body|string | 是  | 无 | 代理请求body 体（页码参数必须使用{{pageIndex}}占位符） |
+*/
+
+type ProxyRquest struct {
+	Url             string                                        `json:"url"  validate:"required"`
+	Method          string                                        `json:"method" validate:"required"`
+	Headers         map[string]string                             `json:"headers"`
+	Body            json.RawMessage                               `json:"body" validate:"required"`
+	PageIndexPath   string                                        `json:"pageIndexPath"` //页码参数路径，例如：$.data.pageIndex
+	MiddlewareFuncs apihttpprotocol.MiddlewareFuncsRequestMessage `json:"-"`             // 请求中间件函数列表，一般可以使用动态脚本生成
+}
+type ProxyResponse struct {
+	DataPath         string `json:"dataPath"  validate:"required"`
+	BusinessCodePath string `json:"businessCodePath"` //业务成功标识路径，例如：$.code
+	BusinessOkCode   string `json:"businessOkCode"`   //业务成功标识值，例如：0
+	//	BusinessOkJson   json.RawMessage                                `json:"businessOkJson"`   //业务成功标识json字符串，例如：{"code":0}
+	MiddlewareFuncs apihttpprotocol.MiddlewareFuncsResponseMessage `json:"-"` // 请求中间件函数列表，一般可以使用动态脚本生成
+}
+type Settings struct {
+	Filename        string        `json:"filename" validate:"required"` //导出文件全称如 /static/export/20231018_1547.xlsx
+	FieldMetas      FieldMetas    `json:"fieldMetas"`                   //字段映射信息{"id":"ID","name":"姓名"}
+	Interval        time.Duration `json:"interval"`
+	DeleteFileDelay time.Duration `json:"deleteFileDelay"`
+}
+
+type ExportApiIn struct {
+	ProxyRquest   ProxyRquest   `json:"proxyRequest" validate:"required"`  //请求数据参数
+	ProxyResponse ProxyResponse `json:"proxyResponse" validate:"required"` //响应数据参数
+	Settings      Settings      `json:"settings" validate:"required"`      //配置信息
+}
+
+const (
+	maxLoopTimes = 5000 //最大循环次数，防止死循环
+)
--- go.mod
+++ go.mod
@@ -9,33 +9,53 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/spf13/cast v1.10.0
 	github.com/stretchr/testify v1.10.0
-	github.com/suifengpiao14/jsonpathmap v0.0.0-20250808103556-59896d07c330
+	github.com/suifengpiao14/apihttpprotocol v0.0.4
 	github.com/tidwall/gjson v1.18.0
 	github.com/tidwall/sjson v1.2.5
 	github.com/xuri/excelize/v2 v2.9.0
-	gitlab.huishoubao.com/gopackage/apihttpprotocol v0.0.1
 )
 
 require (
+	github.com/bytedance/sonic v1.11.6 // indirect
+	github.com/bytedance/sonic/loader v0.1.1 // indirect
+	github.com/cloudwego/base64x v0.1.4 // indirect
+	github.com/cloudwego/iasm v0.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.8 // indirect
+	github.com/gin-contrib/sse v0.1.0 // indirect
+	github.com/gin-gonic/gin v1.10.1 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/goccy/go-json v0.10.2 // indirect
+	github.com/gopherjs/gopherjs v1.17.2 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/jtolds/gls v4.20.0+incompatible // indirect
+	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
 	github.com/leodido/go-urn v1.4.0 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/mohae/deepcopy v0.0.0-20170929034955-c48cc78d4826 // indirect
+	github.com/pelletier/go-toml/v2 v2.2.2 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/richardlehane/mscfb v1.0.4 // indirect
 	github.com/richardlehane/msoleps v1.0.4 // indirect
 	github.com/rogpeppe/go-internal v1.11.0 // indirect
+	github.com/smarty/assertions v1.16.0 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.0 // indirect
+	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
+	github.com/ugorji/go/codec v1.2.12 // indirect
 	github.com/xuri/efp v0.0.0-20240408161823-9ad904a10d6d // indirect
 	github.com/xuri/nfp v0.0.0-20240318013403-ab9948c2c4a7 // indirect
+	golang.org/x/arch v0.8.0 // indirect
 	golang.org/x/crypto v0.33.0 // indirect
 	golang.org/x/net v0.34.0 // indirect
 	golang.org/x/sys v0.30.0 // indirect
 	golang.org/x/text v0.22.0 // indirect
+	google.golang.org/protobuf v1.34.1 // indirect
 	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
+	moul.io/http2curl v1.0.0 // indirect
 	resty.dev/v3 v3.0.0-beta.3 // indirect
 )
